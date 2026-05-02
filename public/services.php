0176cbe2a9507e193f4b2be37e951646e55965fa
AtanuMx <davidatanuinedu@gmail.com>
2026-05-02 17:59:18 +0100

    done working on the service page, hero banner section

---
+++ public/services.php
@@ -0,0 +1,29 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..\/');
+$dotenv->load();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome - <?php echo $_ENV['APP_NAME'] ?? 'Procity' ?></title>
+    <link rel="stylesheet" href="<?php echo $_ENV['APP_ENV'] == 'dev' ? './css/dev_styles.css' : './css/styles.css' ?>" />
+    <link rel="shortcut icon" href="./assets/images/favicon.png" type="image/*">
+    <!-- Browser Iconify Library: https://icon-sets.iconify.design/ -->
+    <script src="https://cdn.jsdelivr.net/npm/iconify-icon@3.0.2/dist/iconify-icon.min.js"></script>
+</head>
+
+<body>
+    <?php include_once './components/header.html' ?>
+    <main class="min-h-screen">
+        <?php include_once './components/services/service-banner.html' ?>
+
+    </main>
+    <?php include_once './components/footer.html' ?>
+    <?php include_once './components/home/chatbox.html' ?>
+</body>
+
+</html>
