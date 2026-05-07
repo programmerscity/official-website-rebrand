b46044a82532f9e1d27f891d4be8c1e0a9cd5a00
ikechukwuchristian252-ui <ikechukwuchristian252@gmail.com>
2026-05-07 15:34:15 +0100

    Done with the featured projects section and i have linked it to the index.php file

--- public/index.php
+++ public/index.php
@@ -29,6 +29,8 @@ $dotenv->load();
         <?php include_once './components/home/choose-path.html' ?>
         <?php include_once './components/home/services.html' ?>
         <?php include_once './components/home/why-choose-us.html' ?>
+        <?php include_once './components/home/featured-projects.html' ?>
+
 
     </main>
     <?php include_once './components/footer.html' ?>
