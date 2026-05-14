f0b3e8ffc60256ec23025927378f16de88ec175f
destinybrotobor <destinybrotos@programmerscity.com>
2026-05-14 16:40:27 +0100

    refactor; created the basic structure

--- public/contact-us.php
+++ public/contact-us.php
@@ -1,6 +1,6 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..\/');
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 ?>
 <!DOCTYPE html>
@@ -9,7 +9,7 @@ $dotenv->load();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome - <?php echo $_ENV['APP_NAME'] ?? 'Procity' ?></title>
+    <title>Contact Us - <?php echo $_ENV['APP_NAME'] ?? 'Procity' ?></title>
     <link rel="stylesheet" href="<?php echo $_ENV['APP_ENV'] == 'dev' ? './public/css/dev_styles.css' : './public/css/styles.css' ?>" />
     <link rel="shortcut icon" href="./public/assets/images/favicon.png" type="image/*">
     <!-- Browser Iconify Library: https://icon-sets.iconify.design/ -->
@@ -25,8 +25,9 @@ $dotenv->load();
 <body class="bg-main-theme text-secondary font-inter">
     <?php include_once './components/header.html' ?>
     <main class="min-h-screen">
-       <!-- Other PHP Included Page Contents -->
-
+        <?php include_once './components/contact-us/contact-hero.html' ?>
+        <?php include_once './components/contact-us/quick-info.html' ?>
+        <?php include_once './components/contact-us/contact-form-section.html' ?>
     </main>
     <?php include_once './components/footer.html' ?>
     <?php include_once './components/home/chatbox.html' ?>
@@ -35,6 +36,9 @@ $dotenv->load();
     <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
     <script>
         AOS.init();
+
+        // Set the anchor tag with hyper-reffernce (href) "./" to active
+        document.querySelectorAll('a[href="./"]').forEach(el => el.classList.add('active'));
     </script>
 </body>
 
