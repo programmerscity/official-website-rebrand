3bf014608a48e010f3763a7697c0f730f9a45355
ikechukwuchristian252-ui <ikechukwuchristian252@gmail.com>
2026-05-15 13:23:45 +0100

    WIP: Done the portfolio card section

--- public/portfolio.php
+++ public/portfolio.php
@@ -1,6 +1,6 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..\/');
 $dotenv->load();
 ?>
 <!DOCTYPE html>
@@ -9,7 +9,7 @@ $dotenv->load();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome - <?php echo $_ENV['APP_NAME'] ?? 'Procity' ?></title>
+    <title>Portfolio - <?php echo $_ENV['APP_NAME'] ?? 'Procity' ?></title>
     <link rel="stylesheet" href="<?php echo $_ENV['APP_ENV'] == 'dev' ? './public/css/dev_styles.css' : './public/css/styles.css' ?>" />
     <link rel="shortcut icon" href="./public/assets/images/favicon.png" type="image/*">
     <!-- Browser Iconify Library: https://icon-sets.iconify.design/ -->
@@ -25,14 +25,8 @@ $dotenv->load();
 <body class="bg-main-theme text-secondary font-inter">
     <?php include_once './components/header.html' ?>
     <main class="min-h-screen">
-        <?php include_once './components/home/hero.html' ?>
-        <?php include_once './components/home/choose-path.html' ?>
-        <?php include_once './components/home/services.html' ?>
-        <?php include_once './components/home/why-choose-us.html' ?>
-        <?php include_once './components/home/blog-section.html' ?>
-        <?php include_once './components/home/what-our-client-says.html' ?>
-        <?php include_once './components/home/our-process.html' ?>
-        <?php include_once './components/home/featured-projects.html' ?>
+        <?php include_once './components/portfolio/project-portfolio.html' ?>
+
     </main>
     <?php include_once './components/footer.html' ?>
     <?php include_once './components/home/chatbox.html' ?>
@@ -41,9 +35,6 @@ $dotenv->load();
     <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
     <script>
         AOS.init();
-
-        // Set the anchor tag with hyper-reffernce (href) "./" to active
-        document.querySelectorAll('a[href="./"]').forEach(el => el.classList.add('active'));
     </script>
 </body>
 
